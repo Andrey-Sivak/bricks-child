ec2728e4097ab3a602373a345e883832e056fd69
Andrey Sivak <andrey.sivak.dev@gmail.com>
2026-01-26 15:46:40 +0100

    chore(theme): adjust .prett .js formatting for consistency

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,52 +4,52 @@
  */
 
 module.exports = {
-    // Formatting options
-    useTabs: true,
-    tabWidth: 4,
-    printWidth: 80,
-    semi: true,
-    singleQuote: true,
-    trailingComma: 'es5',
-    bracketSpacing: true,
-    bracketSameLine: false,
-    arrowParens: 'always',
-    endOfLine: 'lf',
+	// Formatting options
+	useTabs: true,
+	tabWidth: 4,
+	printWidth: 80,
+	semi: true,
+	singleQuote: true,
+	trailingComma: 'es5',
+	bracketSpacing: true,
+	bracketSameLine: false,
+	arrowParens: 'always',
+	endOfLine: 'lf',
 
-    // WordPress specific
-    parenSpacing: true,
+	// WordPress specific
+	parenSpacing: true,
 
-    // Override for specific file types
-    overrides: [
-        {
-            files: '*.json',
-            options: {
-                useTabs: false,
-                tabWidth: 2,
-                printWidth: 100,
-            },
-        },
-        {
-            files: [ '*.yml', '*.yaml' ],
-            options: {
-                useTabs: false,
-                tabWidth: 2,
-            },
-        },
-        {
-            files: '*.md',
-            options: {
-                useTabs: false,
-                tabWidth: 2,
-                printWidth: 100,
-                proseWrap: 'always',
-            },
-        },
-        {
-            files: [ '*.css', '*.scss' ],
-            options: {
-                singleQuote: false,
-            },
-        },
-    ],
+	// Override for specific file types
+	overrides: [
+		{
+			files: '*.json',
+			options: {
+				useTabs: false,
+				tabWidth: 2,
+				printWidth: 100,
+			},
+		},
+		{
+			files: [ '*.yml', '*.yaml' ],
+			options: {
+				useTabs: false,
+				tabWidth: 2,
+			},
+		},
+		{
+			files: '*.md',
+			options: {
+				useTabs: false,
+				tabWidth: 2,
+				printWidth: 100,
+				proseWrap: 'always',
+			},
+		},
+		{
+			files: [ '*.css', '*.scss' ],
+			options: {
+				singleQuote: false,
+			},
+		},
+	],
 };
